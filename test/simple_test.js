7bc01226caf5950e7f07403e001dc472ed8b8307
Bichi Kim <bichi@live.co.kr>
2020-03-31 23:02:28 +0900

    update

--- test/simple_test.js
+++ test/simple_test.js
@@ -1,7 +1,7 @@
-import assert from 'assert';
+import assert from 'assert'
 
 describe('simple', () => {
   it('should be sane', () => {
-    assert.equal(true, !false);
-  });
-});
+    assert.equal(true, !false)
+  })
+})
